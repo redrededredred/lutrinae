f103903d0614ea37d3d458da887fead3f29ecf1b
pissgoblin <pissgoblin@lel.com>
2022-10-06 10:31:23 +0200

    Update bot.py
    Added basic structure

--- bot.py
+++ bot.py
@@ -1,10 +1,29 @@
 import pyautogui as pyg
+import keyboard, time
 
+class Bot:
+    def __init__(self):
+        # used for visual detection later
+        self.bop = "bop.png"
+        self.bite = "bite.png"
+        self.fish_caught = 0
+    def fish(self):
+
+        while not keyboard.is_pressed("q"):
+            print("Exit anytime by pressing: \t \"Q\"")
+            time.sleep(2)
+            print("Fishing...")
+            # fishing logic here
+    def quit(self):
+        print("exiting...")
+        exit(0)
 
 # entry point
 
+
 if __name__ == "__main__":
-    test = pyg.screenshot(region=(2,2,2,2))
+    bot = Bot()
+    bot.fish()
 else:
     print("Do not do that!")
     exit(1)
